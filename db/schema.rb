d3e7130e6aadfe18a8478c7e2a67405034a4130d
Tess T Ryan <tesstryan@gmail.com>
2020-04-03 10:28:38 -0600

    Updates gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903195334) do
+ActiveRecord::Schema.define(version: 2015_09_03_195334) do
 
   create_table "loans", force: :cascade do |t|
     t.decimal "funded_amount", precision: 8, scale: 2
