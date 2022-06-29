f72ef15bbef75a15c4fc8e81f1c5e9d5e036f657
ravindhra <ravimoorthy77@gmail.com>
2022-06-29 22:20:22 +0530

    Paymnets debug,warn and logger info details

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,34 +1,51 @@
 class PaymentsController < ApplicationController
-	before_action :set_current_loan
+	# before_action :set_current_loan
 
-	rescue_from ActiveRecord::RecordNotFound do |exception|
-		render json: 'not_found', status: :not_found
-	end
-
+	# rescue_from ActiveRecord::RecordNotFound do |exception|
+	# 	render json: 'not_found', status: :not_found
+	# end
+  
 	def index
-		render json: @loan.payments
+    byebug
+		# render json: @loan.payments
 	end
+  
+  def new
+    @payment = Payment.new
+  end
 
 	def show
-		render json: Payment.find(params[:id])
+		# render json: Payment.find(params[:id])
 	end
 
 	def create 
-		@payment = @loan.payments.build(format_datetime(payment_params))
-		if @payment.save
-			render json: @payment, status: :created
-		else
-			render json: @payment.errors, status: :bad_request
-		end
+    byebug
+    @payment = Payment.new
+    @payment.amount = params.require(:amount)
+    @payment.save
+    redirect_to @payment
+		# @payment = @loan.payments.build(format_datetime(payment_params))
+		# if @payment.save
+		# 	render json: @payment, status: :created
+		# else
+		# 	render json: @payment.errors, status: :bad_request
+		# end
 	end
+
+  def show
+    @payments = Payment.find(1)
+    @payments.attributes.inspect
+    byebug
+    logger.debug "New Payment: #{@payments.attributes.inspect}"
+  end
 
 	private
 
 	def payment_params
-		params
-		.require(:payment)
-		.permit(:amount,:payment_date)
+		# params.require(:amount).permit(:amount1)
 	end
+
+  
 
 	def set_current_loan
 		@loan = Loan.find(params[:loan_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root "welcome#index" , :as => 'home'
   #resources :loans, defaults: {format: :json}
-  constraints format: :json do
-   resources :loans, only: [:index, :show] do
-       resources :payments,only: [:index, :show, :create]
-    end
-   end
+ 
+  # constraints format: :json do
+  #  resources :loans, only: [:index, :show] do
+  #      resources :payments,only: [:index, :show, :create]
+  #   end
+  #  end
+  resources :payments
+  get 'payments/create', :to => 'payments#create', :as => 'create'
+  get 'payments/show', :to => 'payments#show', :as => 'show'
+
+
 end
