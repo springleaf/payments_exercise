49b2be4c532c31b4af4160fc89a906e0ea41c396
Kwadwo Owusu <mr_kwadwo.owusu@yahoo.com>
2023-04-20 11:33:37 -0400

    add logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do |exception|
+    Rails.logger.error(exception.message)
     render json: { errors: ['not_found'] }, status: :not_found
   end
 end
